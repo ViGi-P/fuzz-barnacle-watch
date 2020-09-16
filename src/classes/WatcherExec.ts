d1917866664460e34bada020ccd97f4f6071bee5
Vigi-P <mail@vigneshprasad.com>
2020-09-16 18:13:51 +0530

    Switch child process from promisified exec to spawn

--- src/classes/WatcherExec.ts
+++ src/classes/WatcherExec.ts
@@ -1,9 +1,8 @@
-import chalk from 'chalk';
-import watchman from 'fb-watchman';
-import { promisify } from 'util';
-import { DirChecker } from './DirChecker';
+import { chalk } from "@caporal/core";
+import watchman from "fb-watchman";
+import { DirChecker } from "./DirChecker";
 
-const exec = promisify(require('child_process').exec);
+import { spawn } from "child_process";
 
 interface WatchResponse {
   watch: string;
@@ -15,7 +14,7 @@ interface FileData {
   size: number;
   mtime_ms: number;
   exists: boolean;
-  type: 'd' | 'f';
+  type: "d" | "f";
 }
 
 interface SubscriptionResponse {
@@ -32,13 +31,13 @@ export class Watcher {
 
   static checkWatchmanInstalled() {
     this.client.capabilityCheck(
-      { optional: [], required: ['relative_root'] },
+      { optional: [], required: ["relative_root"] },
       (error) => {
         if (error) {
           this.client.end();
           throw error;
         }
-      }
+      },
     );
   }
 
@@ -54,16 +53,16 @@ export class Watcher {
   }
 
   static usageError = new Error(
-    'Incorrect usage! Check `fbw --help` for usage information'
+    "Incorrect usage! Check `fbw --help` for usage information",
   );
 
   set target(target: string) {
-    if (target === '/') {
-      target = '';
+    if (target === "/") {
+      target = "";
     } else {
-      if (target.indexOf('./') === 0) target = target.slice(2);
-      if (target.indexOf('/') === 0) target = target.slice(1);
-      if (target[target.length - 1] === '/') target = target.slice(0, -1);
+      if (target.indexOf("./") === 0) target = target.slice(2);
+      if (target.indexOf("/") === 0) target = target.slice(1);
+      if (target[target.length - 1] === "/") target = target.slice(0, -1);
     }
 
     if (target.length) {
@@ -84,7 +83,7 @@ export class Watcher {
   }
 
   private deleteWatch(cb: Function) {
-    Watcher.client.command(['watch-del', this.target], (err /*, resp*/) => {
+    Watcher.client.command(["watch-del", this.target], (err /*, resp*/) => {
       if (err) {
         throw err;
       }
@@ -93,10 +92,10 @@ export class Watcher {
   }
 
   private setupExit() {
-    ['SIGINT', 'SIGUSR1', 'SIGUSR2', 'SIGTERM'].forEach((eventType) => {
+    ["SIGINT", "SIGUSR1", "SIGUSR2", "SIGTERM"].forEach((eventType) => {
       process.on(eventType, (eventType) => {
-        Watcher.client.command(['unsubscribe', this.target, this.target], (
-          error /*, resp*/
+        Watcher.client.command(["unsubscribe", this.target, this.target], (
+          error, /*, resp*/
         ) => {
           if (error) {
             throw error;
@@ -105,12 +104,10 @@ export class Watcher {
           this.deleteWatch(
             Watcher.safeExit.bind(
               Watcher,
-              chalk.bold(
-                chalk.whiteBright(
-                  `\n${eventType} triggered, exiting gracefully`
-                )
-              )
-            )
+              chalk.bold.whiteBright(
+                `\n${eventType} triggered, exiting gracefully`,
+              ),
+            ),
           );
         });
       });
@@ -119,29 +116,29 @@ export class Watcher {
 
   private addWatch() {
     Watcher.client.command(
-      ['watch', this.target],
+      ["watch", this.target],
       (error, resp: WatchResponse) => {
         if (error) {
           throw error;
         }
 
         if (resp.warning) {
-          console.warn(chalk.redBright('warn:'), resp.warning);
+          console.warn(chalk.bold.redBright("warn:"), resp.warning);
         }
         this.addSubscription();
-      }
+      },
     );
   }
 
   private addSubscription(relative_path?: string) {
     const sub = {
-      expression: ['allof', ['match', '*']],
-      fields: ['name', 'size', 'mtime_ms', 'exists', 'type'],
+      expression: ["allof", ["match", "*"]],
+      fields: ["name", "size", "mtime_ms", "exists", "type"],
       ...(relative_path ? { relative_root: relative_path } : {}),
     };
 
-    Watcher.client.command(['subscribe', this.target, this.target, sub], (
-      error /*, resp*/
+    Watcher.client.command(["subscribe", this.target, this.target, sub], (
+      error, /*, resp*/
     ) => {
       if (error) {
         this.deleteWatch(Watcher.safeExit.bind(Watcher, error.message));
@@ -150,37 +147,67 @@ export class Watcher {
       Watcher.watchCount++;
     });
 
-    Watcher.client.on('subscription', (resp: SubscriptionResponse) => {
+    Watcher.client.on("subscription", (resp: SubscriptionResponse) => {
       if (resp.subscription !== this.target) return;
 
-      if (resp.is_fresh_instance)
+      if (resp.is_fresh_instance) {
         console.log(
-          chalk.cyanBright('watching:'),
+          chalk.bold.cyanBright("watching:"),
           this.target,
-          chalk.cyanBright('commands:'),
-          this.commands
+          chalk.bold.cyanBright("commands:"),
+          this.commands,
         );
-      else {
+      } else {
         console.log(
-          chalk.magentaBright('changed:'),
+          chalk.bold.magentaBright("changed:"),
           ...resp.files.reduce<string[]>((acc, file) => {
             let res = file.name;
-            if (file.type === 'd') res = chalk.white(`dir:${res}`);
-            if (!file.exists) res = `${chalk.redBright('deleted:')}${res}`;
+            if (file.type === "d") {
+              res = chalk.white(`${chalk.bold("dir:")}${res}`);
+            }
+            if (!file.exists) res = `${chalk.bold.redBright("deleted:")}${res}`;
             acc.push(res);
             return acc;
-          }, [])
+          }, []),
         );
       }
 
       this.commands.forEach(async (command) => {
         console.log(
-          chalk.blueBright('executing:'),
-          chalk.italic(chalk.white(command))
+          chalk.bold.blueBright("executing:"),
+          chalk.italic.white(command),
         );
-        const { stdout, stderr } = await exec(command);
-        if (stderr) console.error(stderr);
-        console.log(stdout);
+        const splitCommands = command.split(" ");
+        const child = spawn(splitCommands[0], splitCommands.slice(1));
+
+        process.stdin.pipe(child.stdin);
+
+        for await (const data of child.stdout) {
+          process.stdout.write(
+            `${chalk.italic.white(`${command}`)} ${
+              chalk.bold.blueBright("stdout:")
+            } ${data}`,
+          );
+        }
+
+        for await (const data of child.stderr) {
+          process.stdout.write(
+            `${chalk.italic.white(`${command}`)} ${
+              chalk.bold.redBright("stderr:")
+            } ${data},`,
+          );
+          // child.kill(9);
+        }
+
+        child.on("close", (code, signal) => {
+          console.log(
+            chalk.italic.white(command),
+            chalk.bold.blueBright(`exit code:`),
+            code,
+            chalk.bold.blueBright(`signal:`),
+            signal,
+          );
+        });
       });
     });
   }
