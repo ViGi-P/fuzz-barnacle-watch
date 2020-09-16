92a5b8024f93d16b1741f6506c8210e8e29dcc4b
Vigi-P <mail@vigneshprasad.com>
2020-09-16 06:37:02 +0530

    Add util to format options

---
+++ src/utils/formatOpts.ts
@@ -0,0 +1,18 @@
+import { ParserTypes } from '@caporal/core';
+
+interface ResObject {
+  [key: string]: string[];
+}
+
+export function formatOpts(targets: ParserTypes[], commands: ParserTypes[]) {
+  const res: ResObject = {};
+
+  targets.forEach((target, i) => {
+    let key = `${target}`;
+    if (key === '.' || key === './' || key === '/') key = '/';
+    if (res[key]) res[key].push(`${commands[i]}`);
+    else res[key] = [`${commands[i]}`];
+  });
+
+  return res;
+}
