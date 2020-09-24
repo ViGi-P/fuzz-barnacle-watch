8203f145edfbbe3bdfe6d055f62921d88fcd2dc9
Vigi-P <mail@vigneshprasad.com>
2020-09-25 00:53:21 +0530

    Add additional labels and exit codes

--- src/utils/exitSignals.ts
+++ src/utils/exitSignals.ts
@@ -1,1 +1,11 @@
-export const exitSignals = ["SIGINT", "SIGUSR1", "SIGUSR2", "SIGTERM"];
+export const exitSignals = [
+  "SIGINT",
+  "SIGUSR1",
+  "SIGUSR2",
+  "SIGTERM",
+  "SIGABRT",
+  "SIGILL",
+  "SIGQUIT",
+  "SIGTSTP",
+  "SIGXFSZ",
+];
--- src/utils/labels.ts
+++ src/utils/labels.ts
@@ -1,6 +1,9 @@
 import { chalk } from "@caporal/core";
 
 export const labels = {
+  exited: chalk.greenBright("EXITED:"),
+  exitStatus: chalk.cyan("STATUS:"),
+  exitSignal: chalk.cyan("SIGNAL:"),
   error: chalk.red("ERROR:"),
   warn: chalk.redBright("WARN:"),
   deleted: chalk.redBright("DELETED:"),
